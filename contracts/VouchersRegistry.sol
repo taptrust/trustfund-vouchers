bbf238b36f3d6b8bb921d20c453a3401c5df2620
James Levy <thelevybreaks@gmail.com>
2018-10-22 15:20:21 -0700

    Adding Project Files
    Adding Truffle project, source files, dependencies, and test cases.
    
    Complete Milestone 1
    
    Revised the addContractVouchers method to revert, rather than capturing
    the value sent for later withdrawal.
    
    Altered test cases to cover this new functionality.
    
    Added negative test cases.
    
    Cleaning up build directory
    
    Build directory should be ignored.
    
    Added required features
    
    Added all required functionality for VouchersUser and VouchersRegistry.
    
    Added BondingCurve contract, for use during test deployment.
    
    No test coverage as of yet.

---
+++ contracts/VouchersRegistry.sol
@@ -0,0 +1,97 @@
+pragma solidity ^0.4.24;
+
+import './openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import './openzeppelin-solidity/contracts/math/SafeMath.sol';
+import './VouchersUser.sol';
+
+contract VouchersRegistry is Ownable{
+    
+    string constant SAFETY_CHECK_PASSED = "safetyCheckPassed";
+    string constant SAFETY_CHECK_FAILED = "safetyCheckFailed";
+    
+    mapping(address => uint) public safetyCheckPassedContracts;
+    mapping(address => uint) public safetyCheckFailedContracts;
+    
+    mapping(bytes32 => uint) public contractVouchersDonorBalance;
+    mapping(bytes32 => uint) public contractVouchersAddressRedeemed;
+    mapping(bytes32 => uint) public contractVouchersRedeemablePerUser;
+    
+    mapping(address => uint) public addressIdentityVerified;
+    
+    uint redemptionGasFee = 1000000;
+    
+    function setContractStatus(address contractAddress, string status, uint checkVersion) public onlyOwner {
+        require(checkVersion > 0);
+        
+        if(stringsEquivelant(status, SAFETY_CHECK_PASSED))
+            SetSafetyCheckPassed(contractAddress,checkVersion);
+        else if(stringsEquivelant(status, SAFETY_CHECK_FAILED))
+            SetSafetyCheckFailed(contractAddress,checkVersion);
+    }
+    
+    function SetSafetyCheckPassed(address contractAddress, uint checkVersion) internal{
+        safetyCheckPassedContracts[contractAddress] = checkVersion;
+        if(safetyCheckFailedContracts[contractAddress] != 0)
+            safetyCheckFailedContracts[contractAddress] = 0;
+    }
+    
+    function SetSafetyCheckFailed(address contractAddress, uint checkVersion) internal{
+        safetyCheckFailedContracts[contractAddress] = checkVersion;
+        if(safetyCheckPassedContracts[contractAddress] != 0)
+            safetyCheckPassedContracts[contractAddress] = 0;
+    }
+	
+	function setAddressIdentityVerified(address userAddress, uint verificationToken) public onlyOwner {
+		addressIdentityVerified[userAddress] = verificationToken;
+	}
+    
+    function stringsEquivelant (string a, string b) internal pure returns (bool){
+       return keccak256(bytes(a)) == keccak256(bytes(b));
+    }
+    
+    event AddressNotPassedSafetyCheckRefund(address refundee, uint refundAmount);
+    
+    function addContractVouchers(address contractAddress, uint redeemablePerUser) public payable {
+        require(safetyCheckPassedContracts[contractAddress] > 0);
+        
+        address donorAddress = msg.sender;
+        bytes32 voucherKey = keccak256(abi.encodePacked(donorAddress, contractAddress));
+        
+        uint currentBalance = contractVouchersDonorBalance[voucherKey];
+        contractVouchersDonorBalance[voucherKey] = SafeMath.add(currentBalance, msg.value);
+        
+        contractVouchersRedeemablePerUser[voucherKey] = redeemablePerUser;
+    }
+    
+    function withdrawContractVouchers(address contractAddress, uint withdrawAmount) public {
+        address donorAddress = msg.sender;
+        bytes32 voucherKey = keccak256(abi.encodePacked(donorAddress, contractAddress));
+        uint currentBalance = contractVouchersDonorBalance[voucherKey];
+        
+        require(currentBalance >= withdrawAmount && withdrawAmount > 0);
+        
+        contractVouchersDonorBalance[voucherKey] = SafeMath.sub(currentBalance, withdrawAmount);
+        msg.sender.transfer(withdrawAmount);
+    }
+	
+	function redeemContractVouchers(address contractAddress, address donorAddress, uint redeemAmount) public {
+		VouchersUser userAddress = VouchersUser(msg.sender);
+		require(addressIdentityVerified[userAddress] > 0);
+		
+		uint gasCost = SafeMath.mul(redemptionGasFee, tx.gasprice);
+		uint requiredAmount = SafeMath.add(redeemAmount, gasCost);
+		
+		bytes32 voucherKey = keccak256(abi.encodePacked(donorAddress, contractAddress, userAddress));
+		uint donorBalance = contractVouchersDonorBalance[voucherKey];
+		require(donorBalance >= requiredAmount);
+		
+		bytes32 userKey = keccak256(abi.encodePacked(donorAddress, contractAddress));
+		uint totalRedemption = SafeMath.add(redeemAmount, contractVouchersAddressRedeemed[userKey]);
+		
+		require(totalRedemption <= contractVouchersRedeemablePerUser[voucherKey]);
+		contractVouchersAddressRedeemed[userKey] = totalRedemption;
+		contractVouchersDonorBalance[voucherKey] = SafeMath.sub(donorBalance,requiredAmount);
+		
+		userAddress.forwardRedeemedVouchers.value(redeemAmount)(contractAddress);
+	}
+}
---
+++ migrations/3_milestone2_migration.js
@@ -0,0 +1,9 @@
+var VouchersRegistry = artifacts.require("./VouchersRegistry.sol");
+var VouchersUser = artifacts.require("./VouchersUser.sol");
+var BondingCurve = artifacts.require("./relevant-community/contracts/BondingCurve.sol");
+module.exports = function(deployer) {
+	deployer.deploy(VouchersRegistry, {overwrite: false}).then(function() {
+		deployer.deploy(VouchersUser, VouchersRegistry.address);
+	});
+	deployer.deploy(BondingCurve);
+};
